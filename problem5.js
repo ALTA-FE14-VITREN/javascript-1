820f7fd9aad13f4a6a08d05f0c558bcb72c554d3
vitrenSilitonga <vitrensilitonga123@gmail.com>
2023-05-04 17:47:02 +0700

    menambah problem 4 dan 5

---
+++ problem5.js
@@ -0,0 +1,10 @@
+let T = 20;
+let r = 4;
+let pi = 3.14;
+
+let luasAlas = pi * r * r;
+let kelilingAlas = 2 * pi * r;
+let luasSelimut = kelilingAlas * T;
+let luasPermukaantabung = 2 * luasAlas + luasSelimut;
+
+console.log("luas permukaan tabung=", luasPermukaantabung);
